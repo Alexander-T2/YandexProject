48e00a6433b6851072356ed74896aecb3885a91f
Alexander-T2 <89840636+Alexander-T2@users.noreply.github.com>
2022-01-11 01:29:52 +0300

    провалена ерархия файлов, попытка добавить текстуры спрайтов через os

---
+++ settings.py
@@ -0,0 +1,20 @@
+level_map1 = [
+    'X                                  ',
+    '               X                   ',
+    '                                   ',
+    '                      X            ',
+    '                      X            ',
+    '                      X      X     ',
+    '                P     XXX          ',
+    '                XX                 ',
+    '                                   ',
+    '                                   ',
+    '                                   ',
+    'X           X                     X',
+    'X           X                     X',
+    'X                                 X',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map1) * tile_size
