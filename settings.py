1469421225beed0f4c5c1bfe6cfd481c78c99094
Alexander-T2 <89840636+Alexander-T2@users.noreply.github.com>
2022-01-10 23:00:27 +0300

    не отображается

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map1 = [
+    'X                                  ',
+    '               X                   ',
+    '                                   ',
+    '                                   ',
+    '                                   ',
+    '                             X     ',
+    '                                   ',
+    '                                   ',
+    '                                   ',
+    '         X                         ',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map1) * tile_size
